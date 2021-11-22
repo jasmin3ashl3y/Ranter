6011116e2ed926854baf2532cda1fcdaaa108bde
Felicity Brammer <felicitybrammer@gmail.com>
2021-11-22 14:51:27 -0700

    initial model associations

--- models/index.js
+++ models/index.js
@@ -3,4 +3,60 @@ const Post = require('./Post')
 const Comment = require('./Comment')
 const Like = require('./Like')
 
+//create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+    through: Like,
+    as: 'liked_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Like,
+    as: 'liked_posts',
+    foreignKey: 'post_id'
+});
+
+Like.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+Like.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Like, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Like, {
+    foreignKey: 'post_id'
+  });
+
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+
+
+
+
 module.exports = { User, Post, Comment, Like }
