f0596f33ed868e3ec4ca1fa09a5b4d690e13f5d0
jasmin3ashl3y <jasmine@ashleyit.com>
2021-11-24 19:56:14 -0500

    update js

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,5 +1,4 @@
 
-
 async function loginFormHandler(event) {
     event.preventDefault();
   
@@ -24,7 +23,47 @@ async function loginFormHandler(event) {
     }
   }
 
+async function signupFormHandler(event) {
+  event.preventDefault();
 
+  //const chooseUsername = document.querySelector('#choose-username-signup').value.trim();
+  const choosePassword = document.querySelector('#choose-password-signup').value.trim();
+  const confirmPassword = document.querySelector('#confirm-password-signup').value.trim();
+
+  function validation(){
+    var chooseUsername = document.getElementById("choose-username-signup");
+         if(username.value.length <= 20 && user.value.length >= 3){
+         }
+         else{
+             alert("Username has to be between 3-20 characters.")
+          }
+         //duplication data list
+         var user = document.getElementById("user");
+         if(user.value == list.value){
+         }
+         else{
+             alert("Username already exists.")
+          }
+}
+
+  if (choosePassword && confirmPassword === true) {
+    const response = await fetch('/api/login', {
+      method: 'get',
+      body: JSON.stringify({
+        chooseUsername,
+        choosePassword,
+        confirmPassword
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/feed');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
 
 
 
