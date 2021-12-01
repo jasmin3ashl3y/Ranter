13a1140bb7cd8963dd82790b7c7b0e65607e7daa
Felicity Brammer <felicitybrammer@gmail.com>
2021-11-30 18:35:08 -0700

    new comments added

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -1,47 +1,44 @@
 const postContainer = document.getElementById('post-feed-results')
 
 
-async function newCommentHandler(commentId) {
+ function newCommentHandler(comment_text, logPostId) {
   
-  const comment_text = document.getElementById("comment-textarea").value
-    console.log(comment_text)  //because text area is in post-view
-
-    const response = await fetch(`/api/comment`, {
+ 
+    
+    return fetch(`/api/comment`, {
       method: 'POST',
       body: JSON.stringify({
-        comment_text
+        comment_text,
+        post_id: logPostId
       }),
       headers: {
         'Content-Type': 'application/json'
       }
     });
-  
-    if (response.ok) {
-      //add the text to the right field and reload
-      console.log('response ok')
-      document.location.reload('/feed');
-      
-    } else {
-      console.log('error')
-    }
+
   }
 
-    
-  postContainer.addEventListener('click', async event => {
-    event.preventDefault()
-    console.log('click')
-    console.log(event.target.tagName)
-    if (event.target.tagName == 'BUTTON') { 
-      
-      console.log('button clicked')
-      const commentBtn = event.target 
-      const commentId = commentBtn.parentNode.dataset.commentId 
-      console.log(commentId)
-      
-      await newCommentHandler(commentId)
-      
-    } 
-  })
+
+
+postContainer.addEventListener('submit', async event => {
+  event.preventDefault()
+  console.log(event.target.dataset.postId);
+  const logPostId = event.target.dataset.postId
+ const commentForm = event.target
+  const comment_text = commentForm.querySelector(".comment-textarea").value
+  console.log(commentForm.querySelector(".comment-textarea").value)
+
+  const result = await newCommentHandler(comment_text, logPostId) 
+  if (result.ok) {
+    console.log('success') 
+  } else {
+    console.log('no success')
+  }
+
+})
+
+
+
   
   
   
