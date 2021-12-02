68d67fd8d6927806648715c48d056587eebbc575
maplesyrupman <maplesyrupman@protonmail.com>
2021-12-02 18:54:21 -0500

    added default url route

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,8 @@ const router = require('express').Router()
 router.use('/api', apiRoutes)
 router.use('/', homeRoutes)
 
+router.use((req, res) => {
+    res.redirect('/login')
+})
+
 module.exports = router
