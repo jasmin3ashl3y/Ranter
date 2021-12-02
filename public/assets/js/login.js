61c3f548551513562d0308e68fd23040b9bcfd09
maplesyrupman <maplesyrupman@protonmail.com>
2021-12-02 18:15:57 -0500

    corrected signup route

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -34,7 +34,7 @@ async function loginFormHandler(event) {
     const password = document.querySelector('#password-signup').value.trim();
 
     if (username && email && password) {
-        const response = await fetch('/api/user/login', {
+        const response = await fetch('/api/user/', {
             method: 'post',
             body: JSON.stringify({
                 username, 
